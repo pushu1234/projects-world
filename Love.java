07811408f71d3f03633de561d23e23ed7c49b9e1
pushpendra rajput <35806125+pushu1234@users.noreply.github.com>
2018-01-25 20:06:30 +0530

    Add files via upload

---
+++ Love.java
@@ -0,0 +1,71 @@
+
+
+import java.util.*;
+
+public class Love {
+  
+    public static void main(String[] args) {
+        
+        String ch;
+        
+       
+       Scanner s=new Scanner(System.in);
+{
+        System.out.println("Ha ekta ji...Finally java download ho hi gya..:)\n Ab 1 ya 2 ya 3 ya 4 se koisa press kro jaan...\n");
+ }     ch=s.nextLine();
+
+        switch (ch)  {
+        case "1":
+            System.out.println("Ok welcome Ekta ji..\n i am glad to have you in my life..mtlb yar aap meri khushi ho yr betu..\n 
+   . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0       0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . 
+ 0  . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 . 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . 
+ 0  . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . 
+ 0  . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . 
+ 0  . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . 
+ 0  . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . 
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0                                           Ekta Ji
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0  . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . 
+ 0  . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . 
+ 0  . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . 
+ 0  . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . 
+ 0  . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . 
+ 0  . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . 
+ 0  . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . 
+ 0  . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . 
+   . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 . . . . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 . . . . . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . . . . . 0 0 0 0 0 . . . . . . . . . . . . . . . . . . . . 
+ 0  . . . . . . . . . . . . . . . . . . . . . 0 0 0 \n ye mera dil he and isme sirf 1 hi nam likha he..you can see that.. \n Apke bin patani kya hoga mera but jab tk ho tb tk bahut pyar krta rahunga..\n Love u shooo much...");
+break;
+
+        case "2":
+            System.out.println("Ha ekta ji...\n All in the name of love..I love to spend time with you and i wish i will be someday yours forever..\n Dont know wheather it will gonna be happen or not but atleast i can try..\n mtlb yar me betu jaan nd all bolna band hi na kr pata hu..nd aap dat te ho tab bhi bat krne ka man krta he betu...bs yhi bolna cahata hu...\n Love u bahut sara..\n You bring me back to life..");
+            break;
+
+        case "3":
+            System.out.println("Acha...Ty again for doing itna hard work of downloading java.....\n Vese to sab keh hi dia he apko but still this is to make you believe that i love u so much saccha wala...\n Mene kabhi apse flirt nahi kia nd na hi kabhi esa socha ki bs apse kuch cahiye...\n I just need you in my life and Mujhe bht se pal apke sath guzarne he...\n And lunch time pr kar liya kro thik he...\n Nd love u...:0");
+            break;
+
+        case "4":
+            System.out.println("Ok...So aap soch rahe ho ki ye sab to mene pehle hi bol rakha he..isme kya naya he..\n But its a new way to share my feelings..\n Yar apse milta hu to esa lgta he ki kbi jau hi na apko chd ke...zindagi bhar betha hi rahu and apse bate karta rahu jaan..\n Aur kya kahu ab..but aap zindagi me kahi bhi raho me pyar krnaa nahi bhul paunga apko ye to baat pakki he...\n love you..:)");
+            break;
+
+        default:
+            System.out.println("Acha..apko bola tha na 1 2 3 ya 4 me se press krne ko...still apne dusra press kia..\n Chlo koi baat nahi..ab kr dia he to thoda pyar kar lete he...\n But sorry Ekta ji..i am java..i dont know how to love but mere Boss Mr.Rajput ne bola he ki Ekta ji ko meri taraf se 1 kiss dena forehead pe...\n So :0 :0 :0 :0 ....mene 4 krdi...\n muaaah...\n love u..");
+
+             
+        }
+    }
+}
+
